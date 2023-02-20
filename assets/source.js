5b18d3d7e47ac99ca0c6085ba6fbf61147da9738
Hoàng Minh Đức <hoangminhduc4125@gmail.com>
2023-02-21 00:59:38 +0700

    addButtonEvent

---
+++ assets/source.js
@@ -0,0 +1,17 @@
+// var modalManage = document.getElementById('modal_modal');
+// const modalOverlay = document.getElementById('modal__overlay_id');  
+// const registerFormControl = document.getElementById('register__form_id');
+// // const loginForm = document.getElementById('login__form_id');
+// function registerForm() {
+//     modalManage.style.display='flex';
+//     registerFormControl.style.display = 'block';
+//     alert("hmmmmm");
+// }
+
+// function loginForm() {
+
+// }
+
+// function closeForm() {
+//     modalManage.style.display = 'none';
+// }
